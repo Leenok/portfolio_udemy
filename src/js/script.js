7d213e59b607e847b0179d41a1f8cda5f5ff819c
Lenok <eseredich8@gmail.com>
2024-06-15 20:47:14 +0300

    feat: add pulse and hrefs

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const closeElement = document.querySelector('.menu__close');
+const menuItems = document.querySelectorAll('.menu__link');
 
 
 hamburger.addEventListener('click', ()=>{
@@ -11,9 +12,16 @@ closeElement.addEventListener('click', ()=>{
     menu.classList.remove('active');
 })
 
+menuItems.forEach((item)=>{
+    item.addEventListener('click', ()=>{
+        menu.classList.remove('active');
+    })
+})
+
 const counters = document.querySelectorAll('.part-count');
 const lines = document.querySelectorAll('.part__view-color');
 
 counters.forEach((item, i) =>{
     lines[i].style.width = item.innerHTML;
 })
+
